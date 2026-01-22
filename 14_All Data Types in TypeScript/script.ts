9265b4bf9d5237f8dbfb2b70cf17321122732e13
pawan2505 <pawanaktu2gmail.com>
2026-01-22 11:18:57 +0530

    template litral

---
+++ 14_All Data Types in TypeScript/script.ts
@@ -0,0 +1,60 @@
+
+// var username:string|number|boolean = "Pawan Maurya";
+
+
+// username = true;
+
+// console.log(typeof username);
+
+
+// var marks: number = 85;
+
+// console.log(marks)
+// var marks: number = 90; // allowed
+
+// console.log(marks)
+
+// With var, redeclaration is allowed 👉 With let or const, redeclaration is not allowed
+
+
+// var productPrice: number = 499.50;
+// var tax: number = 50.25;
+
+// console.log(productPrice + tax);
+
+
+// Note  : 10+"10"
+
+// var num:number = 121;
+// var digit:string = "10";
+
+// console.log(num+digit); // 12110
+
+// console.log(typeof num+digit);
+
+// question : when we add number datatype value with string datatype value then what will be the final datatype of the result?
+
+// when we add number and string then it will convert number to string and then concatenate both values.
+
+// final value will be string type only
+
+// Question : which one is better typscript or javascript? if TypeScript then why?
+
+// Answer : TypeScript is better than JavaScript because TypeScript is a superset of JavaScript that adds static typing and other features to enhance code quality, maintainability, and developer productivity.
+
+
+// convert string number to number
+
+// var num:string = "100";
+// console.log(num);
+// console.log(typeof num);
+
+// var data = Number(num);
+// console.log(data);
+// console.log(typeof data);
+
+var count = 10;
+count = 20;
+// count = "ten"; // Error
+
+// TypeScript automatically understands count is a number 👉 This is called Type Inference
---
+++ 15_Template Literal/script.js
@@ -0,0 +1,11 @@
+// var vaibhavi_age:number = 15;
+// console.log("Your age is : ",vaibhavi_age);
+// console.log("Your age is : " + vaibhavi_age);
+// console.log(`Your age is : ${vaibhavi_age}`);
+// var digit:number = 46873642864;
+// var str = digit.toString();
+// console.log(typeof str);
+// var username : string | null = null;
+// username = "Pawan";
+// username = 23;
+// console.log(username)
