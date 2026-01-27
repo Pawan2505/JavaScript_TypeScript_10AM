afba85147683ad7833a9ecce893c168f3dd2ed71
pawan2505 <pawanaktu2gmail.com>
2026-01-27 15:28:48 +0530

    revision

---
+++ 17_Types Of Operator/script.js
@@ -0,0 +1,45 @@
+// Types Of Operator
+
+// - Arithmetic operators
+// - Assignment operators
+// - Comparison operators
+// - Logical operators
+
+
+// output : 
+
+// console.log()
+// alert('')
+// document.write('')
+
+
+// prompt('') -> always take input as a string -> parseInt();
+
+
+// document.writeln('Hey, How are you?')
+// document.writeln('Hey, How are you?')
+// document.write('hI, How are you?')
+
+// console.log("Hello Bhai!")
+
+
+// alert('jdfjhdsff')
+
+// let username = "Pawan Maurya";
+
+// alert(`Your name is : ${username}`);
+// // alert("Your name is :" + username);
+// alert("Your name is :" + username);
+
+
+// let username = prompt("Enter Your name : ");
+
+// document.writeln(`Your name is : ${username}`)
+
+// let age = parseInt(prompt("Enter Your age : "));
+
+// document.writeln(`Your age is : ${age}`)
+// document.writeln(typeof age);
+
+
+
---
+++ 18_Revision_DataType/script.js
@@ -0,0 +1,76 @@
+
+// let a = 10;
+// let b = 20;
+
+// console.log(a+b);
+
+
+// DataType : in javascript variable is dynamically typed (varible can hold any type of data) -> data type is determined at runtime -> number, string, boolean, object, undefined, null, symbol
+
+
+
+// let age = 20;
+
+// console.log(age)
+// console.log(typeof age);
+
+
+// let username = "Pawan Maurya";
+
+// console.log(username)
+// console.log(typeof username)
+
+
+
+// let price = 23.45;
+
+// console.log(price)
+// console.log(typeof price)
+
+
+// in javascript float, double and integer all are represented by single data type called number
+
+
+
+// let ispresent = true;
+
+// console.log(ispresent);
+// console.log(typeof ispresent);
+
+
+// type of data type in javascript:
+
+// 1. Premitive data type : it store single value
+
+// 1. number
+// 2. string
+// 3. boolean
+// 4. undefined
+// 6. symbol
+// 7. bigInt
+
+// 2. Non-premitive data type : it store collection of values
+// 1. object -> // null , //array
+
+// 2. function
+
+
+// let num;
+
+// console.log(num)
+// console.log(typeof num)
+
+// let value = 23;
+
+// console.log(value)
+
+// value = null;
+
+// console.log(value)
+// console.log(typeof value)
+
+
+// let s1 = Symbol(10);
+// let s2 = Symbol(10);
+
+// console.log(s1==s2)
