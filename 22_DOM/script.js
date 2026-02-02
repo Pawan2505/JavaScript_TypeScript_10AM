51ea876b38a00d4700c06736e52f3359f3f0bf38
pawan2505 <pawanaktu2gmail.com>
2026-02-02 11:11:16 +0530

    dom

---
+++ 22_DOM/script.js
@@ -0,0 +1,59 @@
+
+// function show(){
+// let heading = document.getElementById("heading");
+
+// // heading.innerHTML = `<i>Hey, How are you?</i>`
+// heading.innerText = `<i>Hey, How are you?</i>`
+
+// }
+
+
+// function show(){
+// // let heading = document.getElementsByClassName("heading")[0];
+// let heading = document.getElementsByClassName("heading")[1];
+
+// heading.innerHTML = `<i>Hey, How are you?</i>`
+// // heading.innerText = `<i>Hey, How are you?</i>`
+
+// }
+
+
+// function show(){
+// // let heading = document.getElementsByClassName("heading")[0];
+// // let heading = document.getElementsByClassName("heading")[1];
+// let heading = document.getElementsByTagName("h1")[0];
+
+// heading.innerHTML = `<i>Hey, How are you?</i>`
+// // heading.innerText = `<i>Hey, How are you?</i>`
+
+// }
+
+// function show(){
+// // let heading = document.getElementsByClassName("heading")[0];
+// // let heading = document.getElementsByClassName("heading")[1];
+// // let heading = document.getElementsByTagName("h1")[0];
+// // let heading = document.querySelector("h1");
+// // let heading = document.querySelector("#h1-heading");
+// let heading = document.querySelector(".heading");
+
+// heading.innerHTML = `<i>Hey, How are you?</i>`
+// // heading.innerText = `<i>Hey, How are you?</i>`
+
+// }
+function show(){
+// let heading = document.getElementsByClassName("heading")[0];
+// let heading = document.getElementsByClassName("heading")[1];
+// let heading = document.getElementsByTagName("h1")[0];
+// let heading = document.querySelector("h1");
+// let heading = document.querySelector("#h1-heading");
+// let heading = document.querySelectorAll(".heading")[0];
+// let heading = document.querySelectorAll(".heading")[1];
+// let heading = document.querySelectorAll("#h1-heading")[1]; // not work at id 
+let heading = document.querySelectorAll("h1")[1];
+
+
+
+heading.innerHTML = `<i>Hey, How are you?</i>`
+// heading.innerText = `<i>Hey, How are you?</i>`
+
+}
