b6ce655a960ab0f1b9de6b65c68dc94155ac47eb
pawan2505 <pawanaktu2gmail.com>
2026-02-04 11:15:29 +0530

    revision

---
+++ 23_Revision/script.js
@@ -0,0 +1,134 @@
+
+// Output print method : 
+
+// 1. Using console.log() -> terminal || developer
+// 2. Using alert() -> pop screen pr or browser
+// 3. Using document.writeln() -> ui pr print krnta hai
+
+// Input methods : 
+
+// 1. Using prompt() -> string input
+
+// parseInt(prompt("Enter a number: ")); // for integer input
+
+
+// variable: 
+
+// 1. variable rules :
+//    - should not start with number
+//    - should not contain space
+//    - should not be a keyword
+//    - should be meaningful
+
+
+// variable declaration  keywords :
+//    - var
+//    - let
+//    - const
+
+// Data types : 
+
+// 1. Primitive data types :
+//    - Number
+//    - String
+//    - Boolean
+//    - Undefined
+//    - Null
+//    - Symbol
+//    - BigInt -> 34676915692354n
+
+// 2. Non-primitive data types :
+//    - Object
+//    - Array
+//    - Function
+
+
+//  Typeof operator : to check data type of variable
+
+// 1. Arithmetic operators : +, -, *, /, %, ++, --
+
+// 2. Assignment operators : =, +=, -=, *=, /=, %=
+
+// 3. Comparison operators : ==, ===, !=, !==, >, <, >=, <=
+
+// 4. Logical operators : &&, ||, !
+
+// 5. String concatenation operator : +
+
+
+// Template literals : using backticks `` and ${}
+
+// let name = "John";
+// let age = 25;
+// console.log(`My name is ${name} and I am ${age} years old.`);
+
+
+// Type conversion :
+
+// 1. String to Number : Number(), parseInt(), parseFloat()
+
+// ex : "10" -> 10
+
+// "10" -> string to number
+// Number("10") -> 10
+
+
+// let num = "10";
+
+// console.log(num);
+// console.log(typeof num);
+
+// console.log(Number(num))
+// console.log(typeof Number(num))
+
+
+// Conditional Statements : 
+
+// 1. if statement
+// 2. if...else statement
+// 3. else if ladder
+// 4. switch statement
+
+// Nested if statement
+
+
+
+// let marks = parseFloat(prompt("Enter Your Marks : "));
+
+// if(marks <= 100 && marks >=80){
+//     if(marks>=90){
+//         document.writeln("Grade A");
+//     }else{
+//         document.writeln("Grade B");
+//     }
+// }
+// else if(marks<80 && marks >=60){
+//     if(marks>=70){
+//         document.writeln("Grade B");
+//     }else{
+//         document.writeln("Grade C");
+//     }
+// }else{
+//     if(marks>100 || marks < 0){
+//         document.writeln("Invalid Input!");
+//     }else{
+//         document.writeln("Sorry, You are Faield!");
+//     }
+// }
+
+
+// let data = confirm("Aye you sure to delete it?");
+
+// if(data){
+//     alert("Deleted Successfully!");
+// }else{
+//     alert("cancel");
+// }
+
+
+// call center
+
+// Press 1 : search 
+// Press 2 : booking class
+// Press 3 :  
+
