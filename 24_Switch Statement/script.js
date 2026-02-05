071a614adc11c1340b046304b146815e19d701a0
pawan2505 <pawanaktu2gmail.com>
2026-02-05 10:50:35 +0530

    loop and switch

---
+++ 24_Switch Statement/script.js
@@ -0,0 +1,73 @@
+// switch case
+
+
+// let exp = 1;
+
+// switch(exp){
+//    case exp1:
+//     // code
+//     break; 
+//    case exp2:
+//     // code
+//     break; 
+//    case exp3:
+//     // code
+//     break; 
+//    case exp4:
+//     // code
+//     break; 
+//    case exp5:
+//     // code
+//     break; 
+//    case exp6:
+//     // code
+//     break;
+//     default:
+
+
+// }
+
+
+// let month = parseInt(prompt("Enter Month Number : "));
+
+// switch(month){
+//     case 1:
+//         document.writeln("Jan");
+//         break;
+//     case 2:
+//         document.writeln("Feb");
+//         break;
+//     case 3:
+//         document.writeln("March");
+//         break;
+//     case 4:
+//         document.writeln("April");
+//         break;
+//     case 5:
+//         document.writeln("May");
+//         break;
+//     case 6:
+//         document.writeln("Jun");
+//         break;
+//     case 7:
+//         document.writeln("July");
+//         break;
+//     case 8:
+//         document.writeln("Aug");
+//         break;
+//     case 9:
+//         document.writeln("Sep");
+//         break;
+//     case 10:
+//         document.writeln("Oct");
+//         break;
+//     case 11:
+//         document.writeln("Nov");
+//         break;
+//     case 12:
+//         document.writeln("Dec");
+//         break;
+//     default:
+//         document.writeln("Invalid Entry!");
+// }
+
---
+++ 25_Loop/script.js
@@ -0,0 +1,19 @@
+
+// - while, do-While, for Loop
+
+// Loop : same pattern of code is executed multiple times until a certain condition is met
+
+// A. Entry Controlled Loops : first check the condition then execute the code block
+
+// 1. For Loop : used when range is known 
+    //  . starting point
+    //  . ending point
+    //  . increment / decrement
+
+// 2. while Loop : used when range is not known
+    // . only condition is provided
+
+// B. Exit Controlled Loops : first execute the code block then check the condition
+
+// 3. do-While Loop : same as while loop but at least one execution is guaranteed
+// at first execution condition is not checked
