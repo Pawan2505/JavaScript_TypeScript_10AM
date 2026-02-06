41ff8470efa6f2ff53179c46086490a6c57b4e07
pawan2505 <pawanaktu2gmail.com>
2026-02-06 11:09:45 +0530

    loop

--- 25_Loop/script.js
+++ 25_Loop/script.js
@@ -13,7 +13,97 @@
 // 2. while Loop : used when range is not known
     // . only condition is provided
 
-// B. Exit Controlled Loops : first execute the code block then check the condition
+// B. Exit Controlled Loops : first execute the code block then c 
 
 // 3. do-While Loop : same as while loop but at least one execution is guaranteed
 // at first execution condition is not checked
+
+
+
+// For Loop : 
+
+// 1 to 10 => 55 (1+2+3+4+5+6+7+8+9+10)
+
+// let sum = 0;
+// for(let i = 1; i <=10; i++){
+//     sum+=i;
+// }
+// console.log(sum);
+
+
+
+// for(let i = 1; i <=20; i++){
+   
+//     if(i%2 == 0){
+//         console.log(i);
+//     }
+// }
+
+
+//  Prime Number : 
+
+// 1, itsef completely divisible -> only ( kisi aur number se hi hona chahiye)
+
+// 2 factor
+
+//  10 => 1, 2,5,10
+
+// 2 -> 1, 2
+// 3 -> 1,3
+// 5 -> 1,5
+// 7 -> 1,7
+
+
+
+
+// for(let i = 2; i <= 20; i++){
+//     let count = 0;
+//     for(let j = 1; j <= i; j++){
+//         if(i%j == 0){
+//             count++;
+//         }
+//     }
+
+//     if(count == 2){
+//         console.log(i);
+//     }
+// }
+
+// 10 ? 
+
+// 5 ? 
+
+
+//  while Loop : 
+
+// syntax :
+
+// while(condition){
+//     // code
+// }
+            count = 5
+//  13634 => digit ? 
+//  13634 => sum of digit ? 
+
+// int value =  13634/10
+
+// let value = 13634/10;
+
+// console.log(Math.floor(value));
+
+
+
+// let value = 13634%10;
+// console.log(value)
+
+
+
+// let num = 1363434345;
+// let count_digit = 0;
+
+// while(num>0){
+//     num = Math.floor(num/10);
+//     count_digit++;
+// }
+
+// console.log(count_digit)
